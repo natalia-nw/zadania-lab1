1d75c59bccaea1b16a474bd681e8b0b44c314855
Natalia <natalia.nwx@gmail.com>
2021-02-22 21:34:30 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+#zad 1
+napis = 'pierwsze zajecia'
+print(napis)
+tekst = 'tekst'
+print(tekst)
+a, b = 3, 7
+c = 20.7
+d = 2.21
+z = 3+2j
+print(a)
+print(b)
+print(c)
+print(d)
+print(z)
+print("\n")
+
+#zad 2
+x, y = 8, 4
+dodawanie = x + y
+odejmowanie = x - y
+dzielenie = x / y
+mnozenie = x * y
+print("wynik dodawania %(z1)d+%(z2)d = %(z3)d" %{'z1':x, 'z2':y, 'z3': dodawanie})
+print("wynik odejmowania %(z1)d-%(z2)d = %(z3)d" %{'z1':x, 'z2':y, 'z3': odejmowanie})
+print("wynik dzielenia %(z1)d/%(z2)d = %(z3)d" %{'z1':x, 'z2':y, 'z3': dzielenie})
+print("wynik mnożenia %(z1)d*%(z2)d = %(z3)d" %{'z1':x, 'z2':y, 'z3': mnozenie})
+print("\n")
+
+#zad 3
+z = 3
+z += 2
+print(z)
+z -= 1
+print(z)
+z *= 2
+print(z)
+z /=2
+print(z)
+z **= 3
+print(z)
+z %= 17
+print(z)
+print("\n")
+
+#zad 4
+
+#zad5
+imie = "natalia"
+nazwisko = "wenda"
+print(imie.capitalize())
+print(nazwisko.capitalize())
+print("\n")
+
+#zad 6
+piosenka = "la la la la la"
+print(piosenka.count("la"))
+print("\n")
+
+#zad 7
+cokolwiek = "cokolwiek"
+print(cokolwiek[1], cokolwiek[8])
+print("\n")
+
+#zad 8
+print(piosenka.split(' '))
+print("\n")
+
+#zad 9
+
